a51b67c5e350c2faf3bd66a8d029ef5de07d45ff
Sithum Dilanga <sithum.9dilanga@gmail.com>
2021-01-25 02:13:45 +0530

    cart item design

--- lib/main-pages/cart_page.dart
+++ lib/main-pages/cart_page.dart
@@ -31,12 +31,64 @@ class _CartPageState extends State<CartPage> {
             print(cart.items[0].itemName);
             print('index : ' + index.toString());
             print(cart.items.length);
-            return Card(
-              child: ListTile(
-                title: Text('${cart.items[index].itemName}'),
-                subtitle: Text('${cart.items[index].itemPrice}'),
-                trailing: Icon(Icons.arrow_circle_up),
-                //onTap: () {},
+            return Padding(
+              padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 8.0),
+              child: Card(
+                elevation: 3.0,
+                child: Row(
+                  // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  mainAxisSize: MainAxisSize.max,
+                  children:<Widget> [
+                    Expanded(
+                      flex: 1,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(topLeft: Radius.circular(4.0), bottomLeft: Radius.circular(4.0)),
+                        child: Image.asset('assets/cappuccino.jpg', fit: BoxFit.cover, height: 110,)
+                      ),
+                    ),
+                    SizedBox(width: 8.0),
+                    Expanded(
+                      flex: 2,
+                      child: Container(
+                        height: 110,
+                        //color: Colors.yellow,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.max,
+                          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('Cappuccino', style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w500),),
+                                IconButton(
+                                  icon: Icon(Icons.close), 
+                                  onPressed: (){},
+                                )
+                              ],
+                            ),
+                          Text('#1', style: TextStyle(fontSize: 16.0),),
+                          SizedBox(height: 16.0),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 16.0),
+                            child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: Text('240 LKR', style: TextStyle(fontSize: 18.0, color: Colors.brown[500],  fontWeight: FontWeight.w700),)
+                            ),
+                          ),
+                        ],
+                        ),
+                      ),
+                    ),
+                  ]
+                )
+                
+                /*ListTile(
+                  title: Text('${cart.items[index].itemName}'),
+                  subtitle: Text('${cart.items[index].itemPrice}'),
+                  trailing: Icon(Icons.arrow_circle_up),
+                  //onTap: () {},
+                ),*/
               ),
             );
           }
