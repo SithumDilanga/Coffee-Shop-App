b1c0939cc10be638df9194628c328e75e03fe26a
Sithum Dilanga <sithum.9dilanga@gmail.com>
2021-06-03 15:04:44 +0530

    sliver appbar

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:coffee_shop_app/common/home_buttons.dart';
 import 'package:coffee_shop_app/main-pages/cart_page.dart';
 import 'package:coffee_shop_app/home/nav_drawer.dart';
@@ -11,10 +13,21 @@ import 'package:coffee_shop_app/models/product.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
 
   // Glabal key for show only burger button for navigation drawer
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+  bool _pinned = true;
+  bool _snap = false;
+  bool _floating = false;
+
+  var top = 0.0;
 
   @override
   Widget build(BuildContext context) {
@@ -22,11 +35,122 @@ class Home extends StatelessWidget {
     // list of coffee products
     final coffeeProducts = Provider.of<List<CoffeeProduct>>(context, listen: true) ?? [];
 
+
     return Scaffold(
       // backgroundColor: Colors.transparent,
         key: _scaffoldState,
         drawer: NavDrawer(),
-        body: Material(
+        body: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              // expandedHeight: 100,
+              backgroundColor: Colors.transparent,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: IconButton(
+                    icon: Icon(Icons.shopping_cart),
+                    color: Colors.white,
+                    iconSize: 32.0,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) =>CartPage()));
+                    },
+                  ),
+                ),
+              ],
+              pinned: _pinned,
+              snap: _snap,
+              floating: _floating,
+              expandedHeight: 200.0,
+              flexibleSpace: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+
+                  top = constraints.biggest.height;
+
+                  // print('constraints=' + constraints.toString());
+                  return Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Image.asset(
+                          'assets/home-img.png',
+                          fit: BoxFit.cover,
+                        )
+                      ),
+                      // Text('Coffee Shop')
+                    ],
+                  );
+                  /*FlexibleSpaceBar(
+                    title: Text('Coffee Shop'),
+                    background: Image.asset(
+                      'assets/home-img.png',
+                      fit: BoxFit.cover,
+                    ),
+                  );*/
+                }  
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildListDelegate(
+                <Widget> [
+                  Padding(    // Coffe Button
+                    padding: const EdgeInsets.fromLTRB(70.0, 16.0, 70.0, 0),
+                    child: HomePageMainButtons(
+                      MyIcons.coffeIcon, 
+                      'Order Coffee', 
+                      () {
+
+                        Navigator.of(context).push(_createRoute(CoffeePage()));
+
+                      /*Navigator.of(context).push(
+                        PageRouteBuilder(pageBuilder: (context, animation, _) {
+                          return CoffeePage();
+                        },
+                        opaque: false
+                        ),
+                      );*/
+                      }
+                    )
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(    // Snacks Button
+                    padding: const EdgeInsets.fromLTRB(70.0, 0, 70.0, 0),
+                    child: HomePageMainButtons(
+                      MyIcons.burgerIcon, 
+                      'Order Snacks', 
+                      () {
+                        Navigator.of(context).push(_createRoute(SnacksPage()));
+                      }
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(      // Desserts Button
+                    padding: const EdgeInsets.fromLTRB(70.0, 0, 70.0, 0),
+                    child: HomePageMainButtons(
+                      MyIcons.iceCreamIcon, 
+                      'Order Desserts', 
+                      () {
+                        Navigator.of(context).push(_createRoute(DessertsPage()));
+                      }
+                    ),
+                  ),
+                  SizedBox(height: 24.0,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        'Today Special', 
+                        style: TextStyle(fontSize: 14.0, color: Colors.grey, fontWeight: FontWeight.w700),)
+                    ),
+                  ),
+                  TodaySpecialGrid(productCategory: coffeeProducts,)
+                ]
+              )
+            )
+          ],
+        ),
+
+        /*Material(
           child: SingleChildScrollView(
               child: SingleChildScrollView(
                 //physics: AlwaysScrollableScrollPhysics(),
@@ -126,7 +250,7 @@ class Home extends StatelessWidget {
                 ),
               ),
             ),
-        ),
+        ), */
       );
   }
 }
